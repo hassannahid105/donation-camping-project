29b29707c77bec699673720e4dc5610992e1c713
nahidHassan405 <infosharemail405@gmail.com>
2024-05-28 23:42:14 +0600

    local storage func added

--- src/component/DonationDetails/DonationDetails.jsx
+++ src/component/DonationDetails/DonationDetails.jsx
@@ -5,6 +5,24 @@ const DonationDetails = () => {
   const detailsCard = useLoaderData();
   const findOne = detailsCard.find((one) => one.id === parseFloat(id));
   const { title, image, cardColor } = findOne;
+  const addDonate = [];
+  // ! local storage func
+  const handleAddtocart = () => {
+    const getDonate = JSON.parse(localStorage.getItem("donate"));
+    if (!getDonate) {
+      addDonate.push(findOne);
+      localStorage.setItem("donate", JSON.stringify(addDonate));
+    } else {
+      const isExist = getDonate.find((donate) => donate.id === findOne.id);
+      if (!isExist) {
+        addDonate.push(...getDonate, findOne);
+        localStorage.setItem("donate", JSON.stringify(addDonate));
+      } else {
+        alert("addDonate");
+      }
+    }
+  };
+  console.log(addDonate);
   return (
     <div className="flex items-center justify-center">
       <div className="w-[500px] my-10" style={{ backgroundColor: cardColor }}>
@@ -19,8 +37,11 @@ const DonationDetails = () => {
             Here are the biggest enterprise technology acquisitions of 2021 so
             far, in reverse chronological order.
           </p>
-          <button className="bg-green-400 rounded-md py-1 font-bold text-xl mt-6">
-            Add to cart
+          <button
+            className="bg-green-400 rounded-md py-1 font-bold text-xl mt-6"
+            onClick={handleAddtocart}
+          >
+            Donate
           </button>
         </div>
       </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import { useLoaderData } from "react-router-dom";
 import Donations from "../../component/Donations.jsx/Donations";
+import Hero from "../../component/Header/Hero/Hero";
 
 const Home = () => {
   const allDonations = useLoaderData();
   return (
     <div>
+      <Hero></Hero>
+
       <Donations allDonations={allDonations}></Donations>
     </div>
   );
